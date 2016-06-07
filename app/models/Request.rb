b68abd1c661f98ff70d61617ceb9a6c305cf1e33
skalic.julian <skalic.julian@gmail.com>
2016-06-06 22:48:11 -0300

    Agregar rutina

--- app/models/Request.rb
+++ app/models/Request.rb
@@ -4,7 +4,7 @@ class Request
 
 		req = Net::HTTP::Get.new(path)
 		#req.set_form_data({'pass' => password}) for headers
-    	http = Net::HTTP.new(Constantes.host, Constantes.port)
+    	http = Net::HTTP.new(Constantes.host)
     	http.request(req)
 	end
 
@@ -12,7 +12,7 @@ class Request
 
 		req = Net::HTTP::Post.new(path)
 		#req.set_form_data({'pass' => password}) for headers
-    	http = Net::HTTP.new(Constantes.host, Constantes.port)
+    	http = Net::HTTP.new(Constantes.host)
     	http.request(req)
 	end
 
