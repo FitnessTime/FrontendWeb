f8142f6238286c4368742093ac7e523cd909476b
skalic.julian <skalic.julian@gmail.com>
2016-06-29 21:54:20 -0300

    Refactor de assemblers dto

--- app/helpers/rutinas_handler_request.rb
+++ app/helpers/rutinas_handler_request.rb
@@ -4,7 +4,7 @@ FitnessTime::App.helpers do
 
 	def handle_request_for_all_routines()
 		begin
-			url = "/rutinas?authToken=" + "m3tBwT1uHKcF5zRg3V4cYzc05"
+			url = "/rutinas?authToken=" + current_token["authToken"]
 			return Request.get_request(url)
 		rescue Exception
       		raise Exception, 'Error al conectar con el servidor, intente nuevamente.'
@@ -13,7 +13,7 @@ FitnessTime::App.helpers do
 
 	def handle_request_for_update_routine(rutina)
 		begin
-			url = "/rutinas?authToken=" + "m3tBwT1uHKcF5zRg3V4cYzc05"
+			url = "/rutinas?authToken=" + current_token["authToken"]
 			return Request.put_request(url + "&rutina=" + rutina.to_json)
 		rescue Exception
       		raise Exception, 'Error al conectar con el servidor, intente nuevamente.'
@@ -23,7 +23,7 @@ FitnessTime::App.helpers do
 
 	def handle_request_for_create_routine(rutinaDto)
 		begin
-			url = "/rutinas?authToken=" + "m3tBwT1uHKcF5zRg3V4cYzc05"
+			url = "/rutinas?authToken=" + current_token["authToken"]
 			return Request.post_request(url + "&rutina=" + rutinaDto.to_json)
 		rescue Exception
       		raise Exception, 'Error al conectar con el servidor, intente nuevamente.'
